2a972afe15331f79625fe2d37d16509d6d104617
Trevor Adcock <trevor.adcock@mac.com>
2019-04-18 10:41:37 -0600

    adds snap finger function

--- ThanosJournal/ViewController.swift
+++ ThanosJournal/ViewController.swift
@@ -8,13 +8,17 @@
 
 import UIKit
 
+typealias Soul = String
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         sadDay()
         happyDay()
+        snapFingers(soul: "Carson")
     }
+    var soulStone: [Soul] = []
 
     func sadDay() {
         print("Dear Journal, I killed Gamora today. #spoilers")
@@ -22,6 +26,10 @@ class ViewController: UIViewController {
     
     func happyDay() {
         print("Dear Journal, Today I got all of the infinity stones. I'm going to cleanse half of the universe.")
+    }
+    
+    func snapFingers(soul: Soul) {
+        soulStone.append(soul)
     }
 }
 
